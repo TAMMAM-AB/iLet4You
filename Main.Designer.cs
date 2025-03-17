9fb3fe72a12f1bed46f7e92f84973246f86fefd3
TAMMAM-AB <tammam.albahri@student.shu.ac.uk>
2025-03-17 15:21:26 +0000

    UI

--- Main.Designer.cs
+++ Main.Designer.cs
@@ -31,16 +31,23 @@
             textBox1 = new TextBox();
             tabControl1 = new TabControl();
             tabPage1 = new TabPage();
+            comboBox1 = new ComboBox();
             tabPage2 = new TabPage();
             tabPage3 = new TabPage();
+            radioButton1 = new RadioButton();
+            tabPage4 = new TabPage();
+            richTextBox1 = new RichTextBox();
+            button1 = new Button();
             tabControl1.SuspendLayout();
+            tabPage4.SuspendLayout();
             SuspendLayout();
             // 
             // textBox1
             // 
-            textBox1.Location = new Point(534, 12);
+            textBox1.Location = new Point(467, 9);
+            textBox1.Margin = new Padding(3, 2, 3, 2);
             textBox1.Name = "textBox1";
-            textBox1.Size = new Size(710, 27);
+            textBox1.Size = new Size(622, 23);
             textBox1.TabIndex = 0;
             textBox1.Text = "Search...";
             // 
@@ -49,53 +56,111 @@
             tabControl1.Controls.Add(tabPage1);
             tabControl1.Controls.Add(tabPage2);
             tabControl1.Controls.Add(tabPage3);
-            tabControl1.Location = new Point(275, 76);
+            tabControl1.Controls.Add(tabPage4);
+            tabControl1.Location = new Point(241, 57);
+            tabControl1.Margin = new Padding(3, 2, 3, 2);
             tabControl1.Name = "tabControl1";
             tabControl1.SelectedIndex = 0;
-            tabControl1.Size = new Size(1427, 700);
+            tabControl1.Size = new Size(1249, 525);
             tabControl1.TabIndex = 1;
             // 
             // tabPage1
             // 
-            tabPage1.Location = new Point(4, 29);
+            tabPage1.Location = new Point(4, 24);
+            tabPage1.Margin = new Padding(3, 2, 3, 2);
             tabPage1.Name = "tabPage1";
-            tabPage1.Padding = new Padding(3);
-            tabPage1.Size = new Size(1419, 667);
+            tabPage1.Padding = new Padding(3, 2, 3, 2);
+            tabPage1.Size = new Size(1241, 497);
             tabPage1.TabIndex = 0;
             tabPage1.Text = "Property Details";
             tabPage1.UseVisualStyleBackColor = true;
             // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(1224, 29);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(121, 23);
+            comboBox1.TabIndex = 3;
+            // 
             // tabPage2
             // 
-            tabPage2.Location = new Point(4, 29);
+            tabPage2.Location = new Point(4, 24);
+            tabPage2.Margin = new Padding(3, 2, 3, 2);
             tabPage2.Name = "tabPage2";
-            tabPage2.Padding = new Padding(3);
-            tabPage2.Size = new Size(1419, 667);
+            tabPage2.Padding = new Padding(3, 2, 3, 2);
+            tabPage2.Size = new Size(1241, 497);
             tabPage2.TabIndex = 1;
             tabPage2.Text = "Landlord";
             tabPage2.UseVisualStyleBackColor = true;
             // 
             // tabPage3
             // 
-            tabPage3.Location = new Point(4, 29);
+            tabPage3.Location = new Point(4, 24);
+            tabPage3.Margin = new Padding(3, 2, 3, 2);
             tabPage3.Name = "tabPage3";
-            tabPage3.Padding = new Padding(3);
-            tabPage3.Size = new Size(1419, 667);
+            tabPage3.Padding = new Padding(3, 2, 3, 2);
+            tabPage3.Size = new Size(1241, 497);
             tabPage3.TabIndex = 2;
             tabPage3.Text = "Tenant";
             tabPage3.UseVisualStyleBackColor = true;
             // 
+            // radioButton1
+            // 
+            radioButton1.AllowDrop = true;
+            radioButton1.AutoSize = true;
+            radioButton1.Location = new Point(1121, 13);
+            radioButton1.Name = "radioButton1";
+            radioButton1.Size = new Size(88, 19);
+            radioButton1.TabIndex = 2;
+            radioButton1.TabStop = true;
+            radioButton1.Text = "radioButton";
+            radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage4
+            // 
+            tabPage4.Controls.Add(button1);
+            tabPage4.Controls.Add(richTextBox1);
+            tabPage4.Location = new Point(4, 24);
+            tabPage4.Name = "tabPage4";
+            tabPage4.Padding = new Padding(3);
+            tabPage4.Size = new Size(1241, 497);
+            tabPage4.TabIndex = 3;
+            tabPage4.Text = "Notes";
+            tabPage4.UseVisualStyleBackColor = true;
+            // 
+            // richTextBox1
+            // 
+            richTextBox1.Location = new Point(6, 6);
+            richTextBox1.Name = "richTextBox1";
+            richTextBox1.Size = new Size(1229, 444);
+            richTextBox1.TabIndex = 0;
+            richTextBox1.Text = "";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(581, 468);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 1;
+            button1.Text = "save";
+            button1.UseVisualStyleBackColor = true;
+            // 
             // Main
             // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1902, 1033);
+            ClientSize = new Size(1664, 775);
+            Controls.Add(comboBox1);
+            Controls.Add(radioButton1);
             Controls.Add(tabControl1);
             Controls.Add(textBox1);
+            Margin = new Padding(3, 2, 3, 2);
             Name = "Main";
             Text = "iLet4You";
             WindowState = FormWindowState.Maximized;
             tabControl1.ResumeLayout(false);
+            tabPage4.ResumeLayout(false);
             ResumeLayout(false);
             PerformLayout();
         }
@@ -107,5 +172,10 @@
         private TabPage tabPage1;
         private TabPage tabPage2;
         private TabPage tabPage3;
+        private RadioButton radioButton1;
+        private ComboBox comboBox1;
+        private TabPage tabPage4;
+        private Button button1;
+        private RichTextBox richTextBox1;
     }
 }
